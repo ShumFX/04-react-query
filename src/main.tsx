badd2ae21aaf26086232a8dd563bd042959a7d63
ShumFX <alfadnepr@gmail.com>
2025-08-08 07:33:06 +0300

    work errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './components/App/App';
-import 'modern-normalize';
 import './index.css';
 
 const queryClient = new QueryClient({
